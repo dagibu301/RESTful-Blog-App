67beb657edbd9101277e1de1780a24273cfd2195
Dagibu <davidgiraldobueno@gmail.com>
2019-07-08 01:11:09 +0000

    navbar semantic ui

--- app.js
+++ app.js
@@ -2,20 +2,47 @@ const express    = require('express'),
       app        = express(),
       bodyParser = require('body-parser'),
       mongoose   = require('mongoose'),
-      path       = require('path')
+      path       = require('path');
 
+//APP/CONFIG
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/restful_blog_app", {
      useNewUrlParser: true });
-app.use(bodyParser.urlencoded({extended: true}));
-      
+app.use(bodyParser.urlencoded({extended: true}));     
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+
+//MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+const Blog = mongoose.model('Blog',blogSchema);
+
+// Blog.create({
+// 	title: 'Test',
+// 	image: 'https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+// 	body: 'Hello this is a blog post!'
+// });
+
+//RESTFUL ROUTES 
 app.get('/', (req, res) => {
-    res.send('Home Page!');
-})
+    res.redirect('blogs');
+});
 
-
+app.get('/blogs', (req,res) => {
+	Blog.find({}, (err,blogs) => {
+		if(err){
+			console.log('ERROR!');
+			console.log(err);
+		} else {
+			res.render('index', {blogs: blogs});
+		}
+	});
+	
+});
 // if(process.env.NODE_ENV === 'production'){
 //     app.use(express.static('client/build'));
 
@@ -24,6 +51,6 @@ app.get('/', (req, res) => {
 //     });
 // }
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen( process.env.PORT || 3000, () => {
     console.log("Server is running");
 });
